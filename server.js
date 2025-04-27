9898d7644c7b105d387920981d1b4a4549be3e71
tramex2024 <tramex2024@gmail.com>
2025-04-26 18:49:01 -0600

    Agregado package.json correctamente con dependencias

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import cors from 'cors';
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Habilitar CORS para que cualquier frontend pueda conectarse
 app.use(cors());
